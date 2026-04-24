ce8e3d888c7bd3ed6de1e90f57da458ae2b7fb0f
Amr-maz <almasriamr1@gmail.com>
2026-04-24 12:40:24 +0300

    resolved circular reference issue

--- src/main/java/com/koursekit/config/EmailConfig.java
+++ src/main/java/com/koursekit/config/EmailConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -20,6 +21,7 @@ public class EmailConfig {
     @Value("${email.password}")
     private String pass;
     @Autowired
+    @Lazy
     private JavaMailSender mailSender;
 
     @Bean
